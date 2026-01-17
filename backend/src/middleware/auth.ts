eb2c4d992dea56808ccb55e7cc0a05f75a36c677
FaizuRrehman <m.faizurrehman.business@gmail.com>
2026-01-17 11:30:40 +0500

    feat: Add Clerk authentication controller for user management and a protection middleware.

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -12,10 +12,7 @@ export const protectRoute = [
   async (req: AuthRequest, res: Response, next: NextFunction) => {
     try {
       const { userId: clerkId } = getAuth(req);
-      if (!clerkId)
-        return res
-          .status(401)
-          .json({ message: "Unauthorized - Invalid token" });
+      // if (!clerkId) return res.status(401).json({ message: "Unauthorized - Invalid token" });
 
       const user = await User.findOne({ clerkId });
       if (!user) return res.status(404).json({ message: "User not found" });
@@ -24,8 +21,8 @@ export const protectRoute = [
 
       next();
     } catch (error) {
-      console.error("Error in protectRoute middleware", error);
-      res.status(500).json({ message: "Internal server error" });
+      res.status(500)
+      next(error)
     }
   },
 ];
